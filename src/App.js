656e6cc818aae730dafb1a261bb74e7bb5f1754d
Kaycee Ingram <KayceeIngram33@gmail.com>
2017-12-15 10:48:30 -0700

    Work in progress

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './reset.css';
+import './App.css';
+import logo from './assets/logo.png';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>SHELFIE</h1>
+        </header>
+        <p className="App-intro">
+          <ShelveChoices />
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
+
