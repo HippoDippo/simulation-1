be7d759272b3cedc62bc39d3eafc6a270c02ce10
Kaycee Ingram <KayceeIngram33@gmail.com>
2017-12-15 12:28:47 -0700

    more ids

--- src/components/ShelveChoices.js
+++ src/components/ShelveChoices.js
@@ -6,10 +6,10 @@ class ShelveChoices extends Component {
     return (
       // JSX and React Router (Link ) used below.
       <div className="shelf-choices">
-        <Link to="/a">><h1>Shelf A</h1></Link>
-        <Link to="/b"><h1>Shelf B</h1></Link>
-        <Link to="/b"><h1>Shelf C</h1></Link>
-        <Link to="/d"><h1>Shelf D</h1></Link>
+        <Link to="">><h1>Shelf A</h1></Link>
+        <Link to=""><h1>Shelf B</h1></Link>
+        <Link to=""><h1>Shelf C</h1></Link>
+        <Link to=""><h1>Shelf D</h1></Link>
       </div>
     )
   }
